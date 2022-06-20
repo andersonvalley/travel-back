4af586dda762adc087fd032634a8fd5970c596db
Andrey Mironov <andrey@Andreys-MacBook-Pro.local>
2022-06-21 00:36:41 +0300

    bc

--- index.js
+++ index.js
@@ -1,12 +1,54 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import multer from 'multer'
+import cors from 'cors'
+
+import { checkAuth, handlerValidationErrors } from './utils/index.js'
+import { UserController, PostController } from './controllers/index.js'
+import { registerValidation, loginValidation, postCreateValidation } from './validations/index.js'
+
+mongoose
+  .connect('mongodb+srv://admin:wwwwww@cluster0.s59fl.mongodb.net/blog?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('db connected')
+  })
+  .catch(err => console.log('db error', err))
 
 const app = express()
-
-app.get('/', (req, res) => {
-  res.send('hello world')
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, 'uploads')
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname)
+  },
 })
 
-app.listen(5000, (err) => {
+const upload = multer({ storage })
+app.use(express.json())
+app.use(cors())
+app.use('/uploads', express.static('uploads'))
+
+app.post('/auth/register', registerValidation, handlerValidationErrors, UserController.register)
+app.post('/auth/login', loginValidation, handlerValidationErrors, UserController.login)
+app.get('/auth/me', checkAuth, UserController.getMe)
+
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  })
+})
+
+app.get('/tags', PostController.getLastTags)
+
+app.get('/posts', PostController.getAll)
+app.get('/posts/tags', PostController.getLastTags)
+app.get('/posts/:id', PostController.getOne)
+app.post('/posts', checkAuth, postCreateValidation, handlerValidationErrors, PostController.create)
+app.delete('/posts/:id', checkAuth, PostController.remove)
+app.patch('/posts/:id', checkAuth, postCreateValidation, handlerValidationErrors, PostController.update)
+
+app.listen(5000, err => {
   if (err) {
     return console.log(err)
   }
